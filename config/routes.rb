c90d9840b67256b83c88ef20adb48b106c9b860d
Thuy Linh <thuylinhrndm@yahoo.com>
2015-11-05 20:03:08 +0000

    add devise gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,19 @@
+
 Rails.application.routes.draw do
+  devise_for :users
   resources :posts
 
   resources :users
 
   get 'app/post'
+  get 'signup' => 'users#new'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   #root 'post#index'
-  root 'users#index'
+  root 'users#index' 
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
